ab8ea2e9a1bd2f1d11fc140628f3083a30b6390b
joe <joe_yi@epam.com>
2021-03-08 17:30:27 +0800

    Update test_notebookc.py

--- tests/test_notebookc.py
+++ tests/test_notebookc.py
@@ -7,3 +7,10 @@ def test_convert(capsys):
     notebookc.convert("Joe")
     captured = capsys.readouterr()
     assert "Joe" in captured.out
+
+
+def test_convert_2(capsys):
+    """Correct my_name argument prints"""
+    notebookc.convert("Jack")
+    captured = capsys.readouterr()
+    assert "Jack" in captured.out
